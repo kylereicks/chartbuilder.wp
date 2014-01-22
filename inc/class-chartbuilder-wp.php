357154022fe7713b30a674f0302f962993b51e0f
Kyle Reicks <reicks.kyle@gmail.com>
2014-01-21 22:04:18 -0600

    Upload image via ajax Almost there, still need to refresh the insert media tab

--- inc/class-chartbuilder-wp.php
+++ inc/class-chartbuilder-wp.php
@@ -39,6 +39,7 @@ if(!class_exists('Chartbuilder_WP')){
       add_filter('media_upload_tabs', array($this, 'register_chartbuilder_wp_tab'));
       add_action('media_upload_chartbuilder_wp', array($this, 'chartbuilder_wp_tab_view'));
       add_action('wp_enqueue_media', array($this, 'chartbuilder_wp_admin_scripts'));
+      add_action('wp_ajax_chartbuilder_upload', array($this, 'chartbuilder_upload'));
     }
 
     public function register_chartbuilder_wp_tab($tabs){
@@ -59,6 +60,81 @@ if(!class_exists('Chartbuilder_WP')){
 
     public function chartbuilder_wp_admin_scripts(){
       wp_enqueue_script('chartbuilder-wp-image-upload', CHARTBUILDER_WP_URL . 'js/chartbuilder-wp-image-upload.js', array('jquery', 'underscore', 'backbone', 'plupload'), CHARTBUILDER_WP_VERSION, true);
+    }
+
+    public function chartbuilder_upload(){
+      if(!function_exists('wp_handle_sideload')){
+        require_once(ABSPATH . 'wp-admin/includes/file.php');
+      }
+
+      if(!function_exists('wp_get_current_user')){
+        require_once(ABSPATH . 'wp-includes/pluggable.php');
+      }
+
+      if(!function_exists('wp_generate_attachment_metadata')){
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+      }
+
+      $post_id = $_POST['postID'];
+      $image_data = base64_decode($_POST['imageBase64Data']);
+      $image_title = $_POST['imageTitle'];
+      $image_filename = str_replace(' ', '_', $image_title) . '.png';
+
+      $upload_dir = wp_upload_dir();
+      $year_month = str_replace($upload_dir['basedir'] . '/', '', $upload_dir['path'] . '/');
+      $upload_path = str_replace('/', DIRECTORY_SEPARATOR, $upload_dir['path']) . DIRECTORY_SEPARATOR;
+      $time = time();
+
+      $image_upload = file_put_contents($upload_path . $time . $image_filename, $image_data);
+
+      $file = array();
+      $file['error'] = '';
+      $file['tmp_name'] = $upload_path . $time . $image_filename;
+      $file['name'] = $image_filename;
+      $file['type'] = 'image/png';
+      $file['size'] = filesize($upload_path . $time . $image_title);
+
+      $file_return = wp_handle_sideload($file, array('test_form' => false));
+
+      if(!isset($file_return['error'])){
+        unlink($file['tmp_name']);
+      }
+
+      $attachment = array(
+        'post_mime_type' => $file_return['type'],
+        'post_title' => $image_title,
+        'post_content' => '',
+        'post_status' => 'inherit',
+        'guid' => $file_return['url']
+      );
+      $attachment_id = wp_insert_attachment($attachment, $image_title, $post_id);
+      $attachment_data = wp_generate_attachment_metadata($attachment_id, $file_return['file']);
+
+      foreach($attachment_data['sizes'] as $size => $object){
+        $attachment_data['sizes'][$size]['file'] = $year_month . $attachment_data['sizes'][$size]['file'];
+      }
+
+      $success = wp_update_attachment_metadata($attachment_id, $attachment_data);
+
+      if(false !== $success){
+        wp_send_json_success();
+      }else{
+        $data = array(
+          'post_id' => $post_id,
+          'image_title' => $image_title,
+          'post_image_title' => $_POST['imageTitle'],
+          'file_return' => $file_return,
+          'upload_dir' => $upload_dir,
+          'upload_path' => $upload_path,
+          'image_upload' => $image_upload,
+          'file' => $file,
+          'attachment' => $attachment,
+          'attachment_id' => $attachment_id,
+          'attachment_data' => $attachment_data,
+          'success' => $success
+        );
+        wp_send_json_error($data);
+      }
     }
 
     public function add_update_hook(){
--- js/chartbuilder-wp-image-upload.js
+++ js/chartbuilder-wp-image-upload.js
@@ -1,42 +1,31 @@
-function blobFromBase64Image(imageBase64Data){
-  var byteCharacters = atob(imageBase64Data);
-
-  function charCodeFromCharacter(c){
-    return c.charCodeAt(0);
-  }
-
-  var byteNumbers = Array.prototype.map.call(byteCharacters, charCodeFromCharacter);
-
-  var unit8Data = new Uint8Array(byteNumbers);
-
-  var imageBlob = new Blob([unit8Data], {type: 'image/png'});
-
-  return imageBlob;
-}
-
 function chartbuilderIframeLoaded(){
   jQuery(function($){
-    $('#__wp-uploader-id-0 .media-iframe iframe').contents().find('#downloadImageLink').click(function(e){
-      var imageBase64Data = $(this).attr('href').match(/^(data:image\/png;base64,)(.+$)/);
 
-      var imageBlob = blobFromBase64Image(imageBase64Data[2]);
-
-      var imageAttributes = {
-        file: imageBlob,
-        uploading: true,
-        date: new Date,
-        filename: $(this).attr('download'),
-        menuOrder: 0,
-        uploadedTo: wp.media.model.settings.post.id || 0,
-        type: 'image',
-        subtype: 'png'
-      }
-
-      var attachment = wp.media.model.Attachment.create(imageAttributes);
-
-      wp.Uploader.queue.add(attachment);
-
+    $('#__wp-uploader-id-0 .media-iframe iframe').contents().find('#downloadImageLink').on('click', function(e){
       e.preventDefault();
+
+      var imageBase64Data = $(this).attr('href').match(/^(data:image\/png;base64,)(.+$)/),
+      imageTitle = $('#__wp-uploader-id-0 .media-iframe iframe').contents().find('#chart_title').val(),
+      postID = $('#post_ID').val();
+
+      $.post(
+        ajaxurl,
+        {
+          'action': 'chartbuilder_upload',
+          'imageBase64Data': imageBase64Data[2],
+          'imageTitle': imageTitle,
+          'postID': postID
+        },
+        function(response){
+          if(response.success){
+            console.log('success');
+//            wp.media.editor.get(wpActiveEditor).close();
+//            wp.media.editor.get(wpActiveEditor).open();
+          }else{
+            console.log(response.data);
+          }
+        }
+      );
     });
   });
 }
